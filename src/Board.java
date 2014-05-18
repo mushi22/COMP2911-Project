e302e98c6c042c1b4b0d8a9863a5db55fe0a7594
petertriho <petertrih@gmail.com>
2014-05-18 23:45:51 +1000

    More Board UI Commits
    Playing around with Swing

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,43 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+/*Bookmarks
+ * http://www.eclipse.org/windowbuilder/
+ * http://docs.oracle.com/javase/tutorial/uiswing/layout/visual.html
+ * http://docs.oracle.com/javase/tutorial/uiswing/components/componentlist.html*/
 
 public class Board extends JFrame {
 	public Board(String title) {
 		super(title);
 		
-		// Set layout manager
+		// Set Layout Manager
+		// Will switch to GridBagLayout
+		setLayout(new BorderLayout());
 		
-		//Create Swing components
+		// Create Swing Components
+		final JTextArea textArea = new JTextArea();
+		JButton button = new JButton("TESTING");
 		
-		//
+		// Add Swing components to content pane
+		Container contentPane = getContentPane();
+		
+		contentPane.add(textArea, BorderLayout.CENTER);
+		contentPane.add(button, BorderLayout.SOUTH);
+		
+		// Add Behaviour
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				textArea.append("TESTING\n");
+			}
+			
+		});
 	}
 }
