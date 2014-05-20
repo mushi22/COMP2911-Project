fa09ca2f1d8cd71745b8afb002aa9cdd15aa97a3
sohaibmushtaq <sohaibmushtaq@sohaibmushtaqs-MacBook-Pro.local>
2014-05-20 13:21:18 +1000

    added

--- src/Maze.java
+++ src/Maze.java
@@ -9,32 +9,45 @@ import java.util.ArrayList;
  */
 
 public class Maze {
-	
+	private char[][] maz;
+	private Point start;
+	private Point end;
+	public char[][] getMaz() {
+		return maz;
+	}
+
+
+	public void setMaz(int row, int column) {
+		this.maz = new char[row][column];
+	}
+
+
 	public static void main(String[]args)
     {
     	// dimensions of generated maze
     	int row=20;
     	int column=20;
-    	generateMaze(row, column);
+    	Maze mz = new Maze();
+    	mz.generateMaze(row, column);
     	
     }
  
 	
-	public static void generateMaze(int row, int column){
+	public void generateMaze(int row, int column){
 		// build maze and initialize with only walls
         StringBuilder s = new StringBuilder(column);
         for(int x=0;x<column;x++){
         	s.append('*');
         }	
-        char[][] maz = new char[row][column];
+        setMaz(row, column);
         for(int x=0;x<row;x++) {
-        	maz[x] = s.toString().toCharArray();
+        	getMaz()[x] = s.toString().toCharArray();
         }
         
  
         // select random point and open as start node
-        Point st = new Point((int)(Math.random()*row),(int)(Math.random()*column),null);
-        maz[st.r][st.c] = 'S';
+        start = new Point((int)(Math.random()*row),(int)(Math.random()*column),null);
+        getMaz()[start.r][start.c] = 'S';
         
  
         // iterate through direct neighbors of node
@@ -45,16 +58,16 @@ public class Maze {
         			continue;
         		}	
         		try{
-        			if(maz[st.r+x][st.c+y]=='.') continue;
+        			if(getMaz()[start.r+x][start.c+y]=='.') continue;
         		}catch(Exception e){ // ignore ArrayIndexOutOfBounds
         			continue;
         		}
         		// add eligible points to frontier
-        		frontier.add(new Point(st.r+x,st.c+y,st));
+        		frontier.add(new Point(start.r+x,start.c+y,start));
         	}
         }
  
-        Point last=null;
+        end=null;
         while(!frontier.isEmpty()){
  
         	// pick current node at random
@@ -62,15 +75,15 @@ public class Maze {
         	Point op = cu.opposite();
         	try{
         		// if both node and its opposite are walls
-        		if(maz[cu.r][cu.c]=='*'){
-        			if(maz[op.r][op.c]=='*'){
+        		if(getMaz()[cu.r][cu.c]=='*'){
+        			if(getMaz()[op.r][op.c]=='*'){
  
         				// open path between the nodes
-        				maz[cu.r][cu.c]='.';
-        				maz[op.r][op.c]='.';
+        				getMaz()[cu.r][cu.c]='.';
+        				getMaz()[op.r][op.c]='.';
  
-        				// store last node in order to mark it later
-        				last = op;
+        				// store end node in order to mark it later
+        				end = op;
  
         				// iterate through direct neighbors of node, same as earlier
         				for(int x=-1;x<=1;x++){
@@ -78,7 +91,7 @@ public class Maze {
 				        		if(x==0&&y==0||x!=0&&y!=0)
 				        			continue;
 				        		try{
-				        			if(maz[op.r+x][op.c+y]=='.') continue;
+				        			if(getMaz()[op.r+x][op.c+y]=='.') continue;
 				        		}catch(Exception e){
 				        			continue;
 				        		}
@@ -92,14 +105,14 @@ public class Maze {
  
         	// if algorithm has resolved, mark end node
         	if(frontier.isEmpty()){
-        		maz[last.r][last.c]='E';
+        		getMaz()[end.r][end.c]='E';
         	}
         }
  
 		// print final maze
 		for(int i=0;i<row;i++){
 			for(int j=0;j<column;j++){
-				System.out.print(maz[i][j]);
+				System.out.print(getMaz()[i][j]);
 			}
 			System.out.println();
 		}
