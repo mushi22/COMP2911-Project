e10aeb6d2b50efacfe3d3ca45e5ed044b4d738b2
petertriho <petertrih@gmail.com>
2014-05-30 23:40:11 +1000

    Cleaned up

--- ass3/src/MyStartPanel.java
+++ ass3/src/MyStartPanel.java
@@ -9,13 +9,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextArea;
 
 
 public class MyStartPanel extends JPanel {
 	private static final long serialVersionUID = -1621932320042675210L;
-	private JTextField txtNavigateMazeUsing;
-	private JTextField txtBy;
 	private JLabel lblMazeGame;
+	private JLabel lblNavigateMazeUsing;
+	private JLabel lblCreatedByAndrew;
 
 	/**
 	 * Create the panel.
@@ -28,7 +30,7 @@ public class MyStartPanel extends JPanel {
 		gridBagLayout.columnWidths = new int[]{0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		lblMazeGame = new JLabel(new ImageIcon(menu));
@@ -39,6 +41,7 @@ public class MyStartPanel extends JPanel {
 		add(lblMazeGame, gbc_lblMazeGame);
 		
 		JLabel lblInstructions = new JLabel("Instructions");
+		lblInstructions.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblInstructions.setBackground(Color.BLACK);
 		lblInstructions.setForeground(Color.WHITE);
 		GridBagConstraints gbc_lblInstructions = new GridBagConstraints();
@@ -47,32 +50,21 @@ public class MyStartPanel extends JPanel {
 		gbc_lblInstructions.gridy = 1;
 		add(lblInstructions, gbc_lblInstructions);
 		
-		txtNavigateMazeUsing = new JTextField();
-		txtNavigateMazeUsing.setForeground(Color.WHITE);
-		txtNavigateMazeUsing.setBackground(Color.BLACK);
-		txtNavigateMazeUsing.setHorizontalAlignment(SwingConstants.CENTER);
-		txtNavigateMazeUsing.setEditable(false);
-		txtNavigateMazeUsing.setText("Navigate maze using arrow keys!");
-		GridBagConstraints gbc_txtNavigateMazeUsing = new GridBagConstraints();
-		gbc_txtNavigateMazeUsing.insets = new Insets(0, 0, 5, 0);
-		gbc_txtNavigateMazeUsing.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtNavigateMazeUsing.gridx = 0;
-		gbc_txtNavigateMazeUsing.gridy = 2;
-		add(txtNavigateMazeUsing, gbc_txtNavigateMazeUsing);
-		txtNavigateMazeUsing.setColumns(10);
+		lblNavigateMazeUsing = new JLabel("Navigate maze using arrow keys!");
+		lblNavigateMazeUsing.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNavigateMazeUsing.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblNavigateMazeUsing = new GridBagConstraints();
+		gbc_lblNavigateMazeUsing.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNavigateMazeUsing.gridx = 0;
+		gbc_lblNavigateMazeUsing.gridy = 2;
+		add(lblNavigateMazeUsing, gbc_lblNavigateMazeUsing);
 		
-		txtBy = new JTextField();
-		txtBy.setBackground(Color.BLACK);
-		txtBy.setForeground(Color.WHITE);
-		txtBy.setEditable(false);
-		txtBy.setHorizontalAlignment(SwingConstants.CENTER);
-		txtBy.setText("Created by: Andrew Thanh Tran, Arien Judge, Peter Ho, Sohaib Mushtaq");
-		GridBagConstraints gbc_txtBy = new GridBagConstraints();
-		gbc_txtBy.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtBy.gridx = 0;
-		gbc_txtBy.gridy = 7;
-		add(txtBy, gbc_txtBy);
-		txtBy.setColumns(10);
+		lblCreatedByAndrew = new JLabel("Created by: Andrew Thanh Tran, Arien Judge, Peter Ho, Sohaib Mushtaq");
+		lblCreatedByAndrew.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblCreatedByAndrew = new GridBagConstraints();
+		gbc_lblCreatedByAndrew.gridx = 0;
+		gbc_lblCreatedByAndrew.gridy = 7;
+		add(lblCreatedByAndrew, gbc_lblCreatedByAndrew);
 	}
 
 }
